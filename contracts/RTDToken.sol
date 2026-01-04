6766b3b3596486c1fd7a84d6520824f70aba22fd
ducdzvc139-cyber <ducdzvc139@gmail.com>
2026-01-05 01:02:09 +0700

    RTDToken.sol

--- contracts/RTDToken.sol
+++ contracts/RTDToken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("RealT Demo Token", "RTD") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
 
